d7320c48559845bc663da052f05121e1adf02475
yaacine <gy_zidelmal@esi.dz>
2020-05-14 15:29:17 +0100

    adding sound

---
+++ TP2_Programmation_Avancee/EXO3/app/src/main/java/com/example/exo3_advenced/DemoService.kt
@@ -0,0 +1,106 @@
+package com.example.exo3_advenced
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+
+class DemoService  : IntentService("DemoService") {
+
+
+
+    override fun onHandleIntent(intent: Intent?) {
+        try {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationID = 1001
+            val pIntent = PendingIntent.getService(this, System.currentTimeMillis().toInt(), intent!!, 0)
+
+            val builder = builder(notificationManager, pIntent)
+            var noti = builder!!.build()
+
+            var mediaPlayer = MediaPlayer.create(this, R.raw.azan1)
+            mediaPlayer?.setOnPreparedListener({
+                println("ready")
+            })
+
+            Thread.sleep(6000)
+
+           // mediaPlayer.start()
+            notificationManager.notify(notificationID, noti)
+            Thread.sleep(6000)
+            builder.setProgress(100, 33, false)
+            noti = builder.build()
+            notificationManager.notify(notificationID, noti)
+            Thread.sleep(6000)
+            builder.setProgress(100, 66, false)
+            noti = builder.build()
+            notificationManager.notify(notificationID, noti)
+            Thread.sleep(6000)
+            builder.setProgress(100, 100, false).setContentText("Terminé !")
+            noti = builder.build()
+            notificationManager.notify(notificationID, noti)
+
+        } catch (ex: Exception) {
+
+        }
+
+    }
+
+    private fun builder(nm: NotificationManager, pi: PendingIntent): Notification.Builder? {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val mChannel = NotificationChannel(
+                "ch00", "ch00", NotificationManager.IMPORTANCE_HIGH)
+            nm.createNotificationChannel(mChannel)
+            val builder = Notification.Builder(this,"ch00")
+                .setContentTitle("Service")
+                .setContentText("Notification à partir d'un service").setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pi).setAutoCancel(true)
+                .setOngoing(true)
+                .setProgress(100, 0, false)
+                .setAutoCancel(true)
+            return builder
+
+
+        }else{
+
+            val builder = Notification.Builder(this)
+                .setContentTitle("Service")
+                .setContentText("Notification à partir d'un service").setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pi).setAutoCancel(true)
+                .setOngoing(true)
+                .setProgress(100, 0, false)
+                .setAutoCancel(true)
+            return builder
+
+        }
+
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private fun handleActionFoo(param1: String, param2: String) {
+        // TODO: Handle action Foo
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private fun handleActionBaz(param1: String, param2: String) {
+        // TODO: Handle action Baz
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    companion object {
+        private val ACTION_SERVICE = "com.example.exo3_advenced"
+
+        fun demarrerService(context: Context) {
+            val intent = Intent(context, DemoService::class.java)
+            intent.action = ACTION_SERVICE
+            context.startService(intent)
+        }
+    }
+}
--- TP2_Programmation_Avancee/EXO3/app/src/main/java/com/example/exo3_advenced/MainActivity.kt
+++ TP2_Programmation_Avancee/EXO3/app/src/main/java/com/example/exo3_advenced/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.exo3_advenced
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    val CUSTOM_INTENT = "com.example.exo3_advenced"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val context = this
+        btnSend.setOnClickListener {
+
+            println("flutter")
+            val i = Intent()
+            i.action = CUSTOM_INTENT
+            i.setClass(this, TestReceiver::class.java)
+            context.sendBroadcast(i)
+        }
+        btnStartService.setOnClickListener { DemoService.demarrerService(context) }
     }
 }
